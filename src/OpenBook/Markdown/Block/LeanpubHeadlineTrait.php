57fa70afc83148bf4967b60f60ef6bf5c707b7dc
Oleg Krivtsov <olegkrivtsov@gmail.com>
2016-09-13 23:26:49 +0600

    Using meaningful headline IDs

--- src/OpenBook/Markdown/Block/LeanpubHeadlineTrait.php
+++ src/OpenBook/Markdown/Block/LeanpubHeadlineTrait.php
@@ -12,56 +12,54 @@ namespace OpenBook\Markdown\Block;
  */
 trait LeanpubHeadlineTrait
 {
-    protected $headerIdSeed = 0;
-    
-	/**
-	 * identify a line as a headline
-	 */
-	protected function identify9LeanpubHeadline($line, $lines, $current)
-	{
-		return ($line[0] === '#');
-	}
+    /**
+     * identify a line as a headline
+     */
+    protected function identify9LeanpubHeadline($line, $lines, $current)
+    {
+        return ($line[0] === '#');
+    }
 
-	/**
-	 * Consume lines for a headline
-	 */
-	protected function consume9LeanpubHeadline($lines, $current)
-	{
+    /**
+     * Consume lines for a headline
+     */
+    protected function consume9LeanpubHeadline($lines, $current)
+    {
         $line = $lines[$current];
         preg_match('/^(#+)/', $line, $matches);
-        
+
         $level = strlen($matches[1]);
         if($level<1) $level = 1;
         if($level>6) $level = 6;
-        
+
         $line = substr($line, $level);
         $line = trim($line);
-        
+
         // Parse header id
         if(preg_match('/\{#([\w\d-_]+)\}$/', $line, $matches)) {
             $id = $matches[1];
             $line = substr($line, 0, strlen($line)-strlen($matches[0]));
         } else {
-            $id = 'header-' . ++$this->headerIdSeed;
+            $id = $this->renderAbsy($this->parseInline(trim($line)));
+            $id = preg_replace('/[^\w\d]/u', '_', $id);
         }
-        
+
         $block = [
             'leanpubHeadline',
             'content' => $this->parseInline(trim($line)),
             'level' => $level,
             'id' => $id
         ];
-        
+
         return [$block, $current];
-	}
+    }
 
-	/**
-	 * Renders a headline
-	 */
-	protected function renderLeanpubHeadline($block)
-	{
-		$tag = 'h' . $block['level'];
-		return "<$tag id=\"".$block['id']."\">" . $this->renderAbsy($block['content']) . "</$tag>\n";
-	}
-
+    /**
+     * Renders a headline
+     */
+    protected function renderLeanpubHeadline($block)
+    {
+        $tag = 'h' . $block['level'];
+        return "<$tag id=\"".$block['id']."\">" . $this->renderAbsy($block['content']) . "</$tag>\n";
+    }
 }
